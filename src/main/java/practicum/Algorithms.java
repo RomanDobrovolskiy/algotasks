87d1ae9c83582a8f3f2b4f97b1fad72de5130df1
Lemendez941 <Lemendez941@yandex.ru>
2022-06-29 12:37:04 +0300

    first commit

--- src/main/java/practicum/Algorithms.java
+++ src/main/java/practicum/Algorithms.java
@@ -29,8 +29,24 @@ public class Algorithms {
      * Верните их сумму
      * Список гарантированно содежит элементы
      */
-     public static double maxMinAvr (List<Integer> numbers) {
-        return -1;
+    public static double maxMinAvr(List<Integer> numbers) {
+        int max = numbers.get(0);
+        int min = numbers.get(0);
+        double average = 0;
+        double sum = 0;
+        double sumForAverage = 0;
+        for (Integer i : numbers) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+            sumForAverage += i;
+            average = sumForAverage / numbers.size();
+            sum = min + max + average;
+        }
+        return sum;
     }
 
 
@@ -39,9 +55,18 @@ public class Algorithms {
      * если такого нет, то вернуть первое
      * Массив гарантировано содержит элементы
      */
-    public static Integer max2 (List<Integer> list) {
-
-        return -1;
+    public static Integer max2(List<Integer> list) {
+        int max1 = list.get(0);
+        int max2 = list.get(0);
+        for (Integer i : list) {
+            if (i > max1) {
+                max2 = max1;
+                max1 = i;
+            } else if (max1 > i && i > max2) {
+                max2 =i;
+            }
+        }
+        return max2;
     }
 
     /**
@@ -52,8 +77,20 @@ public class Algorithms {
      * то возвращаться должен массив содержащий два элемента [6, 5]
      */
     public static int[] removeElementFromArray(int[] numbers, int value) {
-
-        return new int[]{};
+        int[] newArray = null;
+        for (int i = 0; i < numbers.length-1; i++) {
+            if (numbers[i] == value) {
+                newArray = new int[numbers.length - 1];
+                for(int index = 0; index < i; index++){
+                    newArray[index] = numbers[index];
+                }
+                for(int j = i; j < numbers.length - 1; j++){
+                    newArray[j] = numbers[j+1];
+                }
+                break;
+            }
+        }
+        return numbers;
     }
 
 
@@ -74,7 +111,7 @@ public class Algorithms {
      * повторяющихся букв разного регистра,
      * при этом в выходной строке остается первая буква,
      * например, "мМммооЛллокКОоо" -> "моЛокО",
-    */
+     */
     public static String removeDublicates(String str) {
 
         return new String();
@@ -97,7 +134,7 @@ public class Algorithms {
      * Строка "А роза упала на лапу Азора" тоже палиндром,
      * а строка "А роза уколола лапу Азора" нет.
      * "A man, a plan, a canal-Panama", тоже палиндром
-     *
+     * <p>
      * (!) Так как запрещены регулярные выражения
      * и методы преобразования регистра символов из java.lang.String
      * обратите внимание таблицу кодов символов UTF-8
@@ -130,29 +167,27 @@ public class Algorithms {
     }
 
 
-
     /**
-     *
      * Выясните являются ли две строки анограммами.
      * Строки являются анограммами, если они состоят из одних и тех же букв
      * Например, слова "кот" и "ток" анограммы, а слова "кот" и  "кит" нет.
-    */
+     */
     public static boolean isAnogramOf(String word, String anogram) {
 
         return false;
     }
 
     /**
-    * Выясните, все ли символы в строке встречаются один раз.
+     * Выясните, все ли символы в строке встречаются один раз.
      * Если строка содержит повторябщиеся символы,
      * то возвращать false, если не содержит - true
      * Нельзя использовать дополнительные структуры данных.
-     *
+     * <p>
      * (!) В этой задаче строка может содержать
      * любой символ из таблицы ASCII (127 символов)
-     *
+     * <p>
      * (!!) Сложность - O(n)
-    * */
+     */
 
     public static boolean hasUniqueChars(String str) {
 
